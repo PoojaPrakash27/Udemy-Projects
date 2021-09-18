4ed9d2711610d113310a156ca21e77002b654e06
poojaprakash0927 <67591524+poojaprakash0927@users.noreply.github.com>
2021-09-18 12:22:33 -0700

    Adding translator.py

---
+++ translator.py
@@ -0,0 +1,11 @@
+from translate import Translator
+
+
+try:
+    with open('original.txt', mode='r') as fp:
+        text = fp.read()
+        translator = Translator(to_lang="ja")
+        print(translator.translate(text))
+except FileNotFoundError as e:
+    print('Check your path!')
+    raise err
